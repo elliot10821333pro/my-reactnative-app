370a87930e6ee3ec9cc364f8fc00e1849aaf0072
elliot10821333pro <92953320+elliot10821333pro@users.noreply.github.com>
2021-11-17 23:23:17 +0000

    Add files via upload

--- App.js
+++ App.js
@@ -1,22 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View ,Button} from 'react-native';
+import Login from './app/Screens/Login';
+import Signup from './app/Screens/Signup';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './app/Screens/Home';
+// import { StyleSheet,View } from 'react-native';
+
 
 export default function App() {
+  const MainNavigator = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>welcome</Text>
-      <Button title="start here"/>
-      <StatusBar style="auto" />
-    </View>
+    
+      <NavigationContainer>
+          <MainNavigator.Navigator screenOptions={{headerShown:false}}>
+          <MainNavigator.Screen name="Login" component={Login}/>
+          <MainNavigator.Screen name="Signup" component={Signup}/>
+          <MainNavigator.Screen name="Home" component={Home}/>
+          </MainNavigator.Navigator>
+      </NavigationContainer>
+    
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container:{
+//     flex:1,
+//     justifyContent:"center",
+//     alignItems:'center',
+//   }
+// })
